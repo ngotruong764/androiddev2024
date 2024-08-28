431fbc81d561ba065af7d509ee83da5230737095
ngotruong764 <truongnq.ba12-180@st.usth.edu.vn>
2024-08-28 15:11:58 +0700

    update prac 4

--- usth_weather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ usth_weather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class ForecastFragment extends Fragment {
     @Override
@@ -18,11 +20,22 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = new View(getContext());
-        view.setBackgroundColor(0x2000FF00);
+        View v = new View(getContext());
+        v.setBackgroundColor(0x2000FF00);
+        // set linear layout
+        LinearLayout linearLayout = new LinearLayout(getContext());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        //
+//        linearLayout.setBackgroundColor(0x2000FF00);
+        //-
+        ImageView cloudy = new ImageView(getContext());
+        cloudy.setImageResource(R.drawable.cloudy);
+        linearLayout.addView(cloudy);
+        linearLayout.addView(v);
+        //
+        return linearLayout;
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_forecast, container, false);
-        return view;
     }
 
 }
